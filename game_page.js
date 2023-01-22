e846726e35aa98f46038a99cda686e455a1ba61b
Colenghi <110833766+Colenghi@users.noreply.github.com>
2023-01-22 07:22:22 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,18 @@
+
+	player1_name = localStorage.getItem("player1_name");
+	player2_name = localStorage.getItem("player2_name");
+
+function send() {
+	
+}
+localStorage.setItem("player1_name", player1_name);
+localStorage.setItem("player2_name", player2_name);
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check()
+{
+	
+}
+
